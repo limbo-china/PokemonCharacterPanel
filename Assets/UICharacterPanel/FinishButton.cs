7c1f4b2b208f5d2fada56b9e3af1ad78b1fcc0d9
ningbowen <limbo_china@163.com>
2022-01-25 00:32:47 +0800

    c

--- Assets/UICharacterPanel/FinishButton.cs
+++ Assets/UICharacterPanel/FinishButton.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,8 +9,9 @@ public class FinishButton : MonoBehaviour
     Button finishButton;
     public PreviewHandler headPreview;
     public PreviewHandler bodyPreview;
+    public PreviewHandler basePreview;
 
-    string path = "./Assets/Resources/Costumes/";
+    string path = "./Assets/Resources/Costumes/costume_new.png";
     // Start is called before the first frame update
     void Start()
     {
@@ -19,32 +21,47 @@ public class FinishButton : MonoBehaviour
 
     void OnFinish()
     {
-        ToPng(headPreview.atlas[0].texture, "head");
-        ToPng(bodyPreview.atlas[0].texture, "body");
+        Texture2D[] textures = { basePreview.atlas[0].texture, headPreview.atlas[0].texture, bodyPreview.atlas[0].texture };
+        ToPng(textures);
     }
-    void ToPng(Texture2D source,string part)
+    void ToPng(Texture2D[] textures)
     {
-        Color[] sourceColor = source.GetPixels();
+        
+        Color[] sourceColor ;
 
-        int width = source.width , height = source.height ;
+        int width = textures[0].width , height = textures[0].height ;
 
         Texture2D newT = new Texture2D(width, height, TextureFormat.ARGB32, false);
         int length = width * height;
         Color[] newColor = new Color[length];
+        
+        int newIndex, sourceIndex;
 
-        int i = 0;
-        for (int y = 0; y < height; y++)
+        for (int i =0; i<textures.Length;i++)
         {
-            int sourceIndex = y * source.width;
-            for (int x = 0; x < width; x++)
+            sourceColor = textures[i].GetPixels();
+            newIndex = 0;
+            for (int y = 0; y < height; y++)
             {
-                newColor[i++] = sourceColor[sourceIndex++];
+                sourceIndex = y * width;
+                for (int x = 0; x < width; x++)
+                {
+                    if (sourceColor[sourceIndex].a != 0)
+                    {
+                        newColor[newIndex++] = sourceColor[sourceIndex++];
+                    }
+                    else
+                    {
+                        newIndex++;
+                        sourceIndex++;
+                    }
+                }
             }
         }
         newT.SetPixels(newColor);
         newT.Apply();
         byte[] bytes = newT.EncodeToPNG();
-        File.WriteAllBytes(path + part + "/" + source.name + "_new.png", bytes);
+        File.WriteAllBytes(path, bytes);
 
     }
 
